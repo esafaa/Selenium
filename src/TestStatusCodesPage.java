8fc6dc2b65b2733ef8e3c9484fdc478321ef0006
esafaa <132300437+esafaa@users.noreply.github.com>
2023-12-04 12:05:07 -0600

    Initial Commit for this project

--- src/TestStatusCodesPage.java
+++ src/TestStatusCodesPage.java
@@ -4,10 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class StatusCodesPage {
+public class TestStatusCodesPage {
 
 
     WebDriver driver;
