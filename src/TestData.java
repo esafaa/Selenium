093096689f615adf3c51c6c3c6c2079ab6364aca
esafaa <132300437+esafaa@users.noreply.github.com>
2023-12-04 11:58:56 -0600

    Initial Commit for this project

---
+++ src/TestData.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.DataProvider;
+
+public class TestData {
+
+    @DataProvider(name = "statusCodes")
+    public Object[][] getStatusCodes() {
+        return new Object[][]{
+                {"200", "This page returned a 200 status code."},
+                {"301", "This page returned a 301 status code."},
+                {"404", "This page returned a 404 status code."},
+                {"500", "This page returned a 500 status code."}
+
+
+        };
+
+    }
+}
