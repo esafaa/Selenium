b2cf8fb45c1c845b430d2928fdf7d99b74e88c34
esafaa <132300437+esafaa@users.noreply.github.com>
2023-12-12 12:29:15 -0600

    First commit practice alerts

---
+++ src/alertsPopupsPractice/Alerts.java
@@ -0,0 +1,27 @@
+package alertsPopupsPractice;
+
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Alerts {
+    public static void main(String[] args) throws InterruptedException{
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+        Alert alert1= driver.switchTo().alert();
+        Thread.sleep(5000);
+        alert1.accept();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
